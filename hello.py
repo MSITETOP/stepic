17eb20e19b76b37c9996d071554d31cd80bd20b7
mrcrook <ka@msite.top>
2018-10-31 18:24:08 +0000

    g

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type','text/plain')])
-    print(environ['QUERY_STRING'].replace('&','<br>').replace('\r\n',''))
-    return [environ['QUERY_STRING'].replace('\r\n', '').replace('&','<br>')]
+    #print(environ['QUERY_STRING'].replace('&','<br>').replace('\r\n',''))
+    return [bytes(environ['QUERY_STRING'].replace('&',"\n"), 'utf-8')]
